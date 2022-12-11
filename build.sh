4372c2ba4eda40461e0fcc378dd210a6c96835ff
xianghua.du <xianghua.du@homepartners.tech>
2022-12-12 00:11:11 +0800

    update

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 REPO_PREFIX="xingba"
 # DockerfileList=$(git diff-tree --no-commit-id --name-only -n 1 HEAD~1..HEAD  -r --diff-filter=ACMR)
 echo $(git diff-tree --no-commit-id --name-only -r --diff-filter=ACMR HEAD)
-DockerfileList=$(git diff-tree --no-commit-id --name-only -r --diff-filter=ACMR HEAD|grep Dockerfile)
+DockerfileList=$(git diff-tree --no-commit-id --name-only -r --diff-filter=ACMR HEAD)
 for i in $DockerfileList
 do
     REPOPATH=$(dirname $i)
