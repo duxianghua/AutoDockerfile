ef900123c49d924ef93495b83aa580e32891dc89
xianghua.du <xianghua.du@homepartners.tech>
2022-12-12 00:00:13 +0800

    test3

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #GET Dockerfile List
 REPO_PREFIX="xingba"
-DockerfileList=$(git diff-tree --no-commit-id --name-only -n 1 HEAD~1..HEAD  -r --diff-filter=ACMR)
+# DockerfileList=$(git diff-tree --no-commit-id --name-only -n 1 HEAD~1..HEAD  -r --diff-filter=ACMR)
+DockerfileList=$(git diff-tree --no-commit-id --name-only -r --diff-filter=ACMR HEAD|grep Dockefile)
 for i in $DockerfileList
 do
     REPOPATH=$(dirname $i)
