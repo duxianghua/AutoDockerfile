2511cd3cccdc99af60f70e5e3c9a89f44b2c80a2
xianghua.du <xianghua.du@homepartners.tech>
2022-12-12 00:28:59 +0800

    update2

--- build.sh
+++ build.sh
@@ -3,11 +3,15 @@
 set -euxo pipefail
 REPO_PREFIX="xingba"
 # DockerfileList=$(git diff-tree --no-commit-id --name-only -n 1 HEAD~1..HEAD  -r --diff-filter=ACMR)
-DockerfileList=$(git diff-tree --no-commit-id --name-only -r --diff-filter=ACMR HEAD|grep Dockerfile)
+DockerfileList=$(git diff-tree --no-commit-id --name-only -r --diff-filter=ACMR HEAD)
 for i in $DockerfileList
 do
+    
     REPOPATH=$(dirname $i)
     DOCKEFILE=${i##*/}
+    if [[ $i != *"Dockerfile"* ]];then
+        continue
+    fi
     TAG=${DOCKEFILE#*.}
     IMAGE_REPO_FULL_URL="${REPO_PREFIX}/${REPOPATH}:${TAG}"
     
