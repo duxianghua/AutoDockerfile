c4659b7aafa00c8b41e096b8598bd99f53afb14a
xianghua.du <xianghua.du@homepartners.tech>
2022-12-11 23:46:01 +0800

    update

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#GET Dockerfile List
+REPO_PREFIX="xingba"
+DockerfileList=$(git diff-tree --no-commit-id --name-only -n 1 HEAD~1..HEAD  -r --diff-filter=ACMR)
+for i in $DockerfileList
+do
+    REPOPATH=$(dirname $i)
+    DOCKEFILE=${i##*/}
+    TAG=${DOCKEFILE#*.}
+    IMAGE_REPO_FULL_URL="${REPO_PREFIX}/${REPOPATH}:${TAG}"
+    
+    echo "docker build -t ${IMAGE_REPO_FULL_URL} -f $(pwd)/${i} $(pwd)/${REPOPATH}"
+    docker build -t ${IMAGE_REPO_FULL_URL} -f ${i} ./${REPOPATH} && docker push ${IMAGE_REPO_FULL_URL}
+done
