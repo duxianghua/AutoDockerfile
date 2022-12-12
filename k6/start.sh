d5fbc0a762b6962bebce91974cd60a4c89fab2d7
xianghua.du <xianghua.du@homepartners.tech>
2022-12-12 10:57:07 +0800

    update

--- k6/start.sh
+++ k6/start.sh
@@ -1,12 +1,19 @@
-#!/bin/sh
+#!/usr/bin/env sh
+set -euxo pipefail
 # K6_PROMETHEUS_RW_TREND_STATS="avg,min,med,max,p(90),p(95)" 
 # K6_PROMETHEUS_RW_TREND_STATS="max" 
-K6_EXEC_PATH=/Users/jack/workspaces/kubeops/demoandpoc/k6/k6
+
+K6_EXEC_PATH=$(which k6)
 TESTID=$(date '+%Y%m%d%H%M%S')
 _INTERVAL=${INTERVAL-60}
+
+
 while true
 do
-    $K6_EXEC_PATH run -q -i 1 /teset/test.js -o xk6-prometheus-rw --tag pronoea=true --tag testid=$TESTID $@
+    $K6_EXEC_PATH run -q -i 1 /test/test.js -o xk6-prometheus-rw --tag pronoea=true --tag testid=$TESTID $@
+    if [[ $? != 0 ]];then
+        exit $?
+    fi
     echo "sleep ${_INTERVAL} ....."
     sleep $_INTERVAL
 done
