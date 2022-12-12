270953ae4acc5ab7e9b1e0a0725404f088a49887
xianghua.du <xianghua.du@homepartners.tech>
2022-12-12 13:58:11 +0800

    update

--- k6/start.sh
+++ k6/start.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env sh
-set -euxo pipefail
+set -euo pipefail
 # K6_PROMETHEUS_RW_TREND_STATS="avg,min,med,max,p(90),p(95)" 
 # K6_PROMETHEUS_RW_TREND_STATS="max" 
-
 K6_EXEC_PATH=$(which k6)
 TESTID=$(date '+%Y%m%d%H%M%S')
 _INTERVAL=${INTERVAL-60}
 
-
+# while true
+# do
+#     $K6_EXEC_PATH run -q -i 1 /test/test.js -o xk6-prometheus-rw --tag pronoea=true --tag testid=$TESTID $@
+#     if [[ $? != 0 ]];then
+#         exit $?
+#     fi
+#     echo "sleep ${_INTERVAL} ....."
+#     sleep $_INTERVAL
+# done
 while true
 do
-    $K6_EXEC_PATH run -q -i 1 /test/test.js -o xk6-prometheus-rw --tag pronoea=true --tag testid=$TESTID $@
+    $K6_EXEC_PATH run -q -i 1 /test/test.js --tag testid=$TESTID $@
     if [[ $? != 0 ]];then
         exit $?
     fi
---
+++ k6/test.js
@@ -0,0 +1,9 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export default function () {
+  const result = http.get('https://test-api.k6.io/public/crocodiles');
+  check(result, {
+    'http response status code is 200': result.status === 200,
+  });
+}
